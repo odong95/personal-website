16b16bd5c0ec8377d267b5cae269f5cb60c6cdbe
Lately <lately@Brians-MacBook-Pro.local>
2021-02-12 02:28:27 -0600

    updated files

--- src/components/App.constants.js
+++ src/components/App.constants.js
@@ -4,7 +4,7 @@ export const AppConstants = {
   facebookUrl: "https://www.facebook.com/brianodong95",
   twitterUrl: "https://twitter.com/skydxnce",
   githubUrl: "https://github.com/odong95", 
-  aboutPic: "/assets/img/about.webp",
+  aboutPic: "/assets/img/about.png",
   aboutMe: "Hi I'm Brian, a full stack web developer based in the Dallas-Fort Worth area. " +
     "I have a passion for using cutting edge technology to create intuitive, responsive user experiences. " +
     "Check out some of my work!"
