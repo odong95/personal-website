0c50957f1a713e31706b4946b001c80cbb03bb45
Lately <odong951@gmail.com>
2021-12-16 23:23:07 -0600

    refactor spotify embedded link

--- src/components/interests/Spotify.js
+++ src/components/interests/Spotify.js
@@ -5,7 +5,7 @@ const Spotify = () => {
   return (
     <div>
       <LogoLink url={AppConstants.spotifyPlaylistUrl} src={AppConstants.spotifyLogo} text="Spotify Playlist" alt="Spotify"/>
-      <iframe src="https://open.spotify.com/embed/playlist/6gcfUnJWZCb3LN0ezLUObI?utm_source=generator" width="100%"
+      <iframe src={AppConstants.spotifyEmbeddedUrl} width="100%"
         height="380" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" />
     </div>
   )
